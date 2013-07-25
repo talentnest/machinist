9d6e0cedabf841c121570c889708b9d327be9d2d
Daniel Vandersluis <dvandersluis@selfmgmt.com>
2013-07-25 17:47:50 -0400

    Handle timezone aware attributes

--- lib/machinist/active_record/lathe.rb
+++ lib/machinist/active_record/lathe.rb
@@ -23,7 +23,19 @@ module Machinist::ActiveRecord
     def assign_attribute(key, value) #:nodoc:
       @assigned_attributes[key.to_sym] = value
 
+      # If we're assigning an AR attribute, use write_attribute in case the direct setter has been overridden
       if @object.attributes.has_key?(key.to_s)
+        if @object.class.send(:create_time_zone_conversion_attribute?, key, @object.class.columns_hash[key.to_s])
+          # If it's a time attribute, we still want to convert it like Rails would
+          time = value
+          unless time.acts_like?(:time)
+            time = time.is_a?(String) ? Time.zone.parse(time) : time.to_time rescue time
+          end
+          zoned_time = time && time.in_time_zone rescue nil
+          zoned_time.change(usec: 0) if zoned_time
+          value = zoned_time || value
+        end
+
         @object.send(:write_attribute, key, value)
       else
         @object.send("#{key}=", value)
